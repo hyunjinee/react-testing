a832fce28771e0a31ea150b006ed62d0b658f778
hyunjin <leehj0110@kakao.com>
2022-06-19 13:16:58 +0900

    feat: snap shot test

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# react-testing
-TDD in React
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/* eslint-disable testing-library/no-container */
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -6,4 +6,18 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe('<App/>', () => {
+  it('renders component correctly', () => {
+    const { container } = render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+
+    expect(linkElement).toBeInTheDocument();
+    // eslint-disable-next-line testing-library/no-container
+    // eslint-disable-next-line testing-library/no-node-access
+    expect(container.getElementsByClassName('App-logo')).toHaveLength(1);
+
+    expect(container).toMatchSnapshot();
+  });
 });

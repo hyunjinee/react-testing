0628a3fe89b7cb35d7b8a2e8e7dce027eab269bb
hyunjin <leehj0110@kakao.com>
2022-06-20 01:23:22 +0900

    test: Button testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+
 import Button from 'components/common/Button';
 import Input from 'components/common/Input';
 import TodoItem from 'components/TodoItem';
 
 function App() {
+  const [todo, setTodo] = useState('');
+  const [todoList, setTodoList] = useState<string[]>([]);
+
+  const addTodo = () => {
+    if (todo) {
+      setTodoList([...todoList, todo]);
+      setTodo('');
+    }
+  };
+
+  const deleteTodo = (index: number) => {
+    let list = [...todoList];
+    list.splice(index, 1);
+    setTodoList(list);
+  };
+
   return (
     <Container>
       <Contents>
-        <TodoItem label="추가된 할 일" />
+        <TodoListContainer>
+          {todoList.map((item, index) => (
+            <TodoItem
+              key={item + index}
+              label={item}
+              onDelete={() => deleteTodo(index)}
+            />
+          ))}
+        </TodoListContainer>
         <InputContainer>
-          <Input placeholder="할 일을 입력해주세요." />
-          <Button label="추가" />
+          <Input
+            placeholder="할 일을 입력해주세요."
+            onChange={(text) => setTodo(text)}
+            value={todo}
+          />
+          <Button label="추가" onClick={addTodo} />
         </InputContainer>
       </Contents>
     </Container>
@@ -34,6 +64,14 @@ const Contents = styled.div`
   padding: 20px;
   border-radius: 8px;
   box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+`;
+
+const TodoListContainer = styled.div`
+  min-width: 350px;
+  height: 400px;
+  overflow-y: scroll;
+  border: 1px solid #bdbdbd;
+  margin-bottom: 20px;
 `;
 
 const InputContainer = styled.div`
--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -3,11 +3,13 @@ import { InputBox } from './styles';
 interface Props {
   readonly placeholder?: string;
   readonly onChange?: (text: string) => void;
+  readonly value?: string;
 }
 
-const Input: React.FC<Props> = ({ placeholder, onChange }) => {
+const Input: React.FC<Props> = ({ placeholder, onChange, value }) => {
   return (
     <InputBox
+      value={value}
       placeholder={placeholder}
       onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
         if (typeof onChange === 'function') {
